b392d465b7af6b634be46eeafe2e7c1f8365e386
Bukay Mila <107389684+bukaymila@users.noreply.github.com>
2022-08-25 13:31:04 +0800

    First iteration

---
+++ ui.py
@@ -0,0 +1,52 @@
+import customtkinter
+from tkinter import *
+from scrapper import main
+
+
+# Set custom tkinter aperaances
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('dark-blue')
+
+# Quitter
+def _quit():
+    window.quit()
+    window.destroy()
+
+# Define customtkinter variable
+window = customtkinter.CTk()
+window.protocol('WM_DELETE_WINDOW', _quit)
+window.title('Google scholar profile scrapper')
+window.resizable(width=False, height=False)
+window.geometry('470x185')
+#window.iconbitmap('')
+
+# Define inputs
+def get_all():
+    url = str(entryinput.get())
+    result = main(url)
+    labelresult['text'] = result
+
+
+# Frame for elements in the window
+frm_form = customtkinter.CTkFrame(pady=10)
+frm_form.pack()
+
+labelinput = customtkinter.CTkLabel(master=frm_form, text = 'Input profile URL (https://scholar.google.com/citations?user=f-F4yywAAAAJ)')
+labelinput.grid(row=0)
+
+labeltips = customtkinter.CTkLabel(master=frm_form, text = 'Be sure the url ends with user code, remove \'&hl=en&oi=ao\' if any')
+labeltips.grid(row=1)
+
+entryinput = customtkinter.CTkEntry(master=frm_form, width=450)
+entryinput.grid(row=2)
+
+labelblank = customtkinter.CTkLabel(master=frm_form, text=' ')
+labelblank.grid(row=3)
+
+button = customtkinter.CTkButton(master=frm_form, text='Run scraper', command=get_all)
+button.grid(row=4)
+
+labelresult = customtkinter.CTkLabel(master=frm_form, text=' ')
+labelresult.grid(row=5)
+
+window.mainloop()
